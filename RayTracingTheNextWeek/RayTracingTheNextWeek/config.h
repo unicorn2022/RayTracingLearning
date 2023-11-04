a02a4186d6879c870e632bb849eb80cb41b054e4
unicorn2022 <496300118@qq.com>
2023-11-04 20:06:09 +0800

    ray tracing the next week：多线程渲染

--- RayTracingTheNextWeek/RayTracingTheNextWeek/config.h
+++ RayTracingTheNextWeek/RayTracingTheNextWeek/config.h
@@ -3,3 +3,6 @@
 /* 画质设置 */
 static const int samples_per_pixel = 100;	// 每个像素的采样次数
 static const int max_depth = 50;			// 最大递归深度
+
+/* 线程设置 */
+static const int thread_cnt = 10;			// 线程数
--- RayTracingTheNextWeek/RayTracingTheNextWeek/main.cpp
+++ RayTracingTheNextWeek/RayTracingTheNextWeek/main.cpp
@@ -1,6 +1,9 @@
 ﻿#pragma warning(disable:4996)
 #include <iostream>
 #include <iomanip>
+#include <ctime>
+#include <thread>
+
 #include "Utils/Utils.h"
 #include "Camera/Camera.h"
 #include "Object/Sphere.h"
@@ -9,7 +12,6 @@
 #include "Material/Dielectric.h"
 #include "Material/Metal.h"
 #include "config.h"
-#include <ctime>
 
 /* 全局参数设置 */
 namespace {
@@ -17,6 +19,7 @@ namespace {
 	const int Image_Width = 400;			// 图片宽度
 	const int Image_Height = 225;			// 图片高度
 	Color data[Image_Width][Image_Height];	// 图片数据
+	int total_completed = 0;				// 已完成的列数
 	const double aspect = 1.0f * Image_Width / Image_Height;	// 宽高比
 
 	/* 世界设置 */
@@ -53,12 +56,8 @@ void AddObjects() {
 	);
 }
 
-void Render() {
-	for (int j = 0; j < Image_Height; j++) {
-		double completed = (j * 100) / Image_Height;
-		std::cerr << "\r已完成: " << std::setw(3) << completed << "%" << std::flush;
-		std::cerr << "\t剩余行数: " << std::setw(3) << Image_Height - j << std::flush;
-
+void Render(int L, int R, bool single) {
+	for (int j = L; j < R; j++) {
 		for (int i = 0; i < Image_Width; i++) {
 			// 每个像素随机采样 samples_per_pixel 次, 并取平均值
 			for (int cnt = 0; cnt < samples_per_pixel; cnt++) {
@@ -77,24 +76,50 @@ void Render() {
 			// gamma 矫正
 			data[i][j].gamma();
 		}
+
+		total_completed++;
+		if(single) std::cerr << "\r" << "已完成: " << total_completed << "/" << Image_Height << ", (" << total_completed * 100 / Image_Height << "%)" << std::flush;
 	}
 }
 
 int main() {
 	freopen("../output.ppm", "w", stdout);
+	
+	auto t = clock();
+	AddObjects();
 
 	// 渲染图片
-	auto t = clock();
-	Render();
-	std::cerr << "\n图片渲染完成, 耗时 " << clock() - t << "ms\n";
-	t = clock();
+	if (thread_cnt == 1) {
+		std::cerr << "单线程模式\n";
+		Render(0, Image_Height, true);
+	}
+	else {
+		std::cerr << "多线程模式, 线程数: " << thread_cnt << "\n";
+		int num = Image_Height / thread_cnt;
+		for (int i = 0; i < thread_cnt; i++) {
+			int L = i * num, R = (i + 1) * num;
+			if(i == thread_cnt - 1) R = Image_Height;
+
+			std::thread ti(Render, L, R, false);
+			ti.detach();
+		}
+	
+		// 监视渲染进度
+		while (total_completed < Image_Height) {
+			std::cerr << "\r" << "已完成: " << total_completed << "/" << Image_Height << ", (" <<  total_completed * 100 / Image_Height << "%)" << std::flush;
+			std::this_thread::sleep_for(std::chrono::milliseconds(200));
+		}
+		std::cerr << "\r" << "已完成: " << Image_Height << "/" << Image_Height << ", (100%)" << std::flush;
+	}
+	std::cerr << "\n图片渲染完成, 耗时 " << (clock() - t) / 1000.0f << "s\n";
 
 	// 输出图片
+	t = clock();
 	std::cout << "P3\n" << Image_Width << " " << Image_Height << "\n255\n";
 	for (int j = Image_Height - 1; j >= 0; j--)
 		for (int i = 0; i < Image_Width; i++)
 			data[i][j].write_color(std::cout);
-	std::cerr << "图片输出完成, 耗时 " << clock() - t << "ms\n";
+	std::cerr << "图片输出完成, 耗时 " << (clock() - t) / 1000.0f << "s\n";
 
 	fclose(stdout);
 	return 0;
